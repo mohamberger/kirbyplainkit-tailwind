1ce5444964efa51fb098e213a6fdbc9fb8c8b71a
mohamberger <info@moritz-hamberger.de>
2020-08-25 19:39:14 +0200

    add template for home page

---
+++ site/templates/home.php
@@ -0,0 +1,9 @@
+<head>
+<?= css('assets/css/main.css') ?>
+</head>
+<?= snippet('navigation') ?>
+<body class="bg-yellow-300">
+
+    <h1 class="font-bold text-6xl text-center mt-24 leading-tight"><?= $page->title() ?></h1>
+
+</body>
