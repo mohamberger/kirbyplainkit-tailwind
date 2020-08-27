d9f3ed4bd6fc55345a8c380969099fc3e49a5f82
mohamberger <info@moritz-hamberger.de>
2020-08-27 15:08:13 +0200

    get partner logos in part-partners.php

--- site/snippets/part-partners.php
+++ site/snippets/part-partners.php
@@ -3,7 +3,7 @@
   <div class="flex justify-between">
     <h2 class="text-2xl font-bold"><?= $part->headline(); ?></h2>
     <ul class="flex flex-wrap w-2/3 justify-end">
-      <?php foreach ($part->images() as $logo): ?>
+      <?php foreach ($part->logos()->toFiles() as $logo): ?>
       <li><img class="h-16 mx-8 mb-16" src="<?= $logo->url() ?>" alt=""></li>
       <?php endforeach ?>
     </ul>
