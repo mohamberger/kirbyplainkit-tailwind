8abd1f5c84f54ac8d268c46f7612565b459316cb
mohamberger <info@moritz-hamberger.de>
2020-08-27 15:16:57 +0200

    beautify

--- site/templates/athlete.php
+++ site/templates/athlete.php
@@ -1,28 +1,20 @@
 <head>
-<?= css('assets/css/main.css') ?>
+  <?= css('assets/css/main.css') ?>
 </head>
 
 <body class="bg-black text-white">
 
-    <h1 class="font-bold text-6xl text-center mt-24 leading-tight"><?= $page->title() ?></h1>
-    <p><?= $page->first_name() ?></p>
-    <p><?= $page->last_name() ?></p>
-    <p><?= $page->profession() ?></p>
-    <p><?= $page->birthday() ?></p>
+  <h1 class="font-bold text-6xl text-center mt-24 leading-tight"><?= $page->title() ?></h1>
+  <p><?= $page->first_name() ?></p>
+  <p><?= $page->last_name() ?></p>
+  <p><?= $page->profession() ?></p>
+  <p><?= $page->birthday() ?></p>
 
-    <?= $page->portrait()->toFile()->resize(50)->url();?>
+  <span>Portraitbild: </span><?= $page->portrait()->toFile()->resize(50)->url();?>
 
-    <?php if ($file = $page->files()->first()): ?>
-    <img src="<?= $file->url() ?>" alt="">
-    <?php endif ?>
-
-
-    <?php foreach ($page->children()->listed() as $part) : ?>
-    <section class="<?= $part->intendedTemplate() ?>">
-      <?php snippet($part->intendedTemplate(), compact('part')) ?>
-    </section>
+  <?php foreach ($page->children()->listed() as $part) : ?>
+  <section class="<?= $part->intendedTemplate() ?>">
+    <?php snippet($part->intendedTemplate(), compact('part')) ?>
+  </section>
   <?php endforeach ?>
-
-
 </body>
-  
