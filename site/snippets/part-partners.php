045a10d1ed06e11db14730bf1d72b68f04527a78
mohamberger <info@moritz-hamberger.de>
2020-08-28 11:09:33 +0200

    add image template in back-end and make logo clickable

--- site/snippets/part-partners.php
+++ site/snippets/part-partners.php
@@ -4,7 +4,7 @@
     <h2 class="text-2xl font-bold"><?= $part->headline(); ?></h2>
     <ul class="flex flex-wrap w-2/3 justify-end">
       <?php foreach ($part->logos()->toFiles() as $logo): ?>
-      <li><img class="h-16 mx-8 mb-16" src="<?= $logo->url() ?>" alt=""></li>
+      <li><?php if($logo->link()->length() > 0): ?><a href="<?= $logo->link() ?>" target="_blank"><?php endif; ?><img class="h-16 mx-8 mb-16" src="<?= $logo->url() ?>" alt="<?= $logo->alt() ?>"></a></li>
       <?php endforeach ?>
     </ul>
   </div>
